bbd398fc2494f3b79d16ab98217b52c8809845bd
bubbabaird <bubbabaird@me.com>
2017-05-24 15:18:19 -0400

    test for +1 game in the database

--- src/test/java/com/theironyard/charlotte/GameTrackerSpringApplicationTests.java
+++ src/test/java/com/theironyard/charlotte/GameTrackerSpringApplicationTests.java
@@ -1,12 +1,18 @@
 package com.theironyard.charlotte;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.Assert.assertEquals;
 
@@ -14,31 +20,54 @@ import static org.junit.Assert.assertEquals;
 @SpringBootTest
 public class GameTrackerSpringApplicationTests {
 
+	MockMvc mockMvc;
+
+	@Autowired
+	WebApplicationContext wap;
+
+	@Autowired
+	UserRepository users;
+
+	@Autowired
+	GameRepository games;
+
+	@Before
+	public void initMethod () {
+		mockMvc = MockMvcBuilders.webAppContextSetup(wap).build();
+	}
+
 	@Test
 	public void contextLoads() {
 	}
 
 	@Test
-	public void whoIsTheUser() {
-		User initialUser = new User();
-		initialUser.setName("Zach");
-		initialUser.setPassword("b00gieMan");
+	public void whoIsTheUser() throws Exception {
+//		User initialUser = new User();
+//		initialUser.setName("Zach");
+//		initialUser.setPassword("b00gieMan");
 
-		String result = initialUser.getName();
+//		String result = initialUser.getName();
+//
+//		assertEquals("Zach", result);
+//		Game initialGame = new Game("MarioKart", "Wii", "Racing", 2001, users.findOne(1));
 
-		assertEquals("Zach", result);
+		// from within the terminal at our project's directory:
+		// `heroku create` -- creates a new heroku website for you
+		// `git push heroku master` -- pushes our latest code to heroku
 
-//		ObjectMapper mapper = new ObjectMapper();
-//
-//		String json = mapper.writeValueAsString(user);
-//
-//		mockMvc.perform(
-//				MockMvcRequestBuilders.post("/user")
-//						.content(json)
-//						.contentType("application/json")
-//		);
-//
-//		Assert.assertTrue(users.count() == 1);
+
+		int originalCount = (int) games.count();
+
+		mockMvc.perform(
+				MockMvcRequestBuilders.post("/add-game")
+						.sessionAttr("userName", "Zach")
+						.param("gameYear", "2001")
+						.param("gameName", "FFVII")
+						.param("gamePlatform", "PS1")
+						.param("gameGenre", "RPG")
+		);
+
+		Assert.assertEquals(originalCount + 1, games.count());
 //	}
 //	}
 //	@Test
